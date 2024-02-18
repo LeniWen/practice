d153f64356a7cc31278cf7ff0222a5621e3d9974
lenianwain <its9527@outlook.com>
2024-02-18 22:44:03 +0800

    progress bar II

--- src/components/progress-bars-II/script.js
+++ src/components/progress-bars-II/script.js
@@ -1,24 +1,42 @@
 /**
+ * @types {Element[]}
+ */
+const barEls = []
+let filledBars = 0
+
+function startTransition(index) {
+  requestAnimationFrame(() => {
+    barEls[index].classList.add('progress-bar-filled')
+  })
+}
+
+/**
  * @param {Element} $element
  * @param {number} initValue
  */
 function progressBar($element, initValue) {
-  if (!$element)
-    return
   const $progressBarEl = document.createElement('div')
   $progressBarEl.classList.add('progress-bar')
+  barEls.push($progressBarEl)
+  if (filledBars + 1 === barEls.length)
+    startTransition(filledBars)
+
+  $progressBarEl.addEventListener('transitionend', () => {
+    filledBars += 1
+    if (filledBars < barEls.length)
+      startTransition(filledBars)
+  })
   const clampedValue = Math.min(100, Math.max(initValue, 0))
   $progressBarEl.style.setProperty('width', `${clampedValue}%`)
-  $progressBarEl.textContent = `${clampedValue}%`
 
   $element.classList.add('progress')
   $element.appendChild($progressBarEl)
 }
-progressBar(document.querySelector('#progress-0'), 0)
-progressBar(document.querySelector('#progress-25'), 25)
-progressBar(document.querySelector('#progress-50'), 50)
-progressBar(document.querySelector('#progress-75'), 75)
-progressBar(document.querySelector('#progress-100'), 100)
-progressBar(document.querySelector('#progress-2'), 2)
-progressBar(document.querySelector('#progress--10'), -10)
-progressBar(document.querySelector('#progress-120'), 120)
+
+const btnEl = document.querySelector('#add-bar')
+btnEl.addEventListener('click', () => {
+  const wrapperEl = document.querySelector('#bars-wrapper')
+  const progressEl = document.createElement('div')
+  wrapperEl.appendChild(progressEl)
+  progressBar(progressEl, Math.floor(Math.random() * 101))
+})
