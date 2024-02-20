9f6d710ce12dbdfee998425a84d195a9e03add5c
lenianwain <its9527@outlook.com>
2024-02-20 13:43:59 +0800

    drop right while

---
+++ src/libs/dropRightWhile.ts
@@ -0,0 +1,8 @@
+export function dropRightWhile<T>(
+  array: Array<T>,
+  predicate: (value: T, index: number, array: Array<T>) => boolean,
+): Array<T> {
+  let i = array.length - 1
+  while (i >= 0 && predicate(array[i], i, array)) i -= 1
+  return array.slice(0, i + 1)
+}
